bd3c098e1c12e298df82a927a0032b9a63b576e2
Francisco Marao <francisco.cnmarao@sp.senac.br>
2019-11-04 09:51:50 -0300

    fix problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Game from './components/Game';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import Game from './components/Game';
 
 // SERVICES
 import userService from './services/userService';
