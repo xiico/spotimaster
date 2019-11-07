88a27849c1b947360dac97d580149eddadea45c8
Francisco Marao <francisco.cnmarao@sp.senac.br>
2019-11-07 18:00:01 -0300

    update user

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -9,5 +9,16 @@ export default {
     console.log('user_service_id:',id);
     let res = await axios.get(`/api/user?id=${id}`);
     return res.data;
+  },  
+  update: async (user) => {
+    // console.log('user_service_id:',id);
+    const data = {
+      name: user.display_name,
+      id: user.id,
+      points: user.points,
+      picture: user.images[0].url
+    }
+    let res = await axios.put(`/api/user?id=${user.id}`, data);
+    return res.data;
   }
 }
