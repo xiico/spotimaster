6a6c460a33276d1f045a4e070bdc3b0f2a2584e9
francisco marão <francisco.marao@hotmail.com>
2019-11-18 21:37:44 -0300

    flat alt and new icons

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -22,19 +22,19 @@ export default function Leaderboard(props) {
 
     return (
         <div className="user_list">
-            {users ? users.map((u,i) => {
+            {users ? users.map((u, i) => {
                 return (
                     <div className="leaderboard_profile" key={u.id}>
-                        <div className="leaderboard_profile_rank">{(i+1)}</div>
+                        <div className="leaderboard_profile_rank">{(i + 1)}</div>
                         <div className="leaderboard_picture" >
                             <a target="_blank" rel="noopener noreferrer" href={`https://open.spotify.com/user/${u.id}`}>
-                                {( u.picture ? <img className="leaderboard_profile_picture" alt="Profile" src={u.picture} /> : <ProfilePicture size={'big'} /> )}
+                                {(u.picture ? <img className="leaderboard_profile_picture" alt="Profile" src={u.picture} /> : <ProfilePicture size={'big'} />)}
                             </a>
                         </div>
                         <div className="leaderboard_profile_info">
                             <span className="leaderboard_profile_name">{u.name}</span>
-                            <div className="leaderboard_profile_points"><span>{ format((u.scores[0] || {}).points || 0, ' ')}</span></div>
-                            <div className="leaderboard_profile_stats" style={!u.scores[0] ? hide : null } >
+                            <div className="leaderboard_profile_points"><span>{format((u.scores[0] || {}).points || 0, ' ')}</span></div>
+                            <div className="leaderboard_profile_stats" style={!u.scores[0] ? hide : null} >
                                 <span>{(u.scores[0] || {}).hits} of {(u.scores[0] || {}).total}</span>
                                 <span>Max Combo: {(u.scores[0] || {}).maxcombo}</span>
                                 <Flag code={u.country} />
